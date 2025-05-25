e1180b604cd855fca5cd86ba522bf7a820476bd9
C15HOT <wvxp@mail.ru>
2025-05-25 16:43:55 +0300

    add llm

--- app/bot/bot.py
+++ app/bot/bot.py
@@ -229,7 +229,7 @@ async def process_event_details(message: types.Message, state: FSMContext):
         print(result)
         print(type(result))
         await state.update_data(event_data=result)
-        await state.update_date(data='123')
+        await state.update_data(data={'data': 'data'})
         await state.set_state(EventCreation.waiting_for_commit)
         # # Отправляем ответ пользователю
         # await message.reply(result)
